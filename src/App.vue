11f5aba59de16befcc83853a3f83cdd8195a3bc5
Muhammad ijaz <75518471+Muhammad-ijazCRT@users.noreply.github.com>
2022-02-22 20:51:37 +0500

    vue-js-simple-router-routes
    vue-js-simple-router-routes

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+<div id="app">
+  <h1>Hello App!</h1>
+  
+    <router-link to="/">Go to Home</router-link> <br> <br>
+    <router-link to="/contact">Contact us</router-link> <br> <br>
+    <router-link to="/about">About us</router-link>
+  
+  <router-view />
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
